37bfe9abe2c817bfa136382ab41c7c9038aefe63
Amaury Caballero <amcaballero2023@gmail.com>
2021-05-18 02:16:51 -0400

    generating shuffled groups

--- groups_divisor/lib/groups_divisor.dart
+++ groups_divisor/lib/groups_divisor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+
 import 'models/groups_models.dart';
 import 'models/student_model.dart';
 
@@ -39,7 +40,53 @@ Future<List<String>> getTopics(String locFile) async {
   return topics;
 }
 
-List<Group> generateGroups(
-    int quantity, List<Student> students, List<String> topics) {
-  return <Group>[];
+List<Group> generateGroups(int groupQuantity, List<Student> originalStudents,
+    List<String> originalTopics) {
+  List<Group> groups = [];
+  List<Student> students = List.from(originalStudents);
+  List<String> topics = List.from(originalTopics);
+  if (isBigger(students.length, groupQuantity) &&
+      isBigger(topics.length, groupQuantity)) {
+    double studentsPerGroup = students.length / groupQuantity;
+    double topicsPerGroup = topics.length / groupQuantity;
+
+    var list = List<int>.generate(groupQuantity, (index) => index + 1);
+    list.shuffle();
+
+    for (var i = 0; i < groupQuantity; i++) {
+      var topicsGroup = <String>[];
+      var studentsGroup = <Student>[];
+
+      Group group = Group(
+        id: list.removeAt(0).toString(),
+        students: studentsGroup,
+        topics: topicsGroup,
+      );
+
+      groups.add(group);
+    }
+
+    for (var i = 0; i < studentsPerGroup; i++) {
+      for (var j = 0; j < groupQuantity; j++) {
+        if (students.isNotEmpty) {
+          Student newStudent = students.removeAt(0);
+
+          groups[j].students.add(newStudent);
+        }
+      }
+    }
+
+    for (var i = 0; i < topicsPerGroup; i++) {
+      for (var j = 0; j < groupQuantity; j++) {
+        if (topics.isNotEmpty) {
+          var newTopic = topics.removeAt(0);
+
+          groups[j].topics.add(newTopic);
+        }
+      }
+    }
+  }
+
+  groups.sort((a, b) => int.parse(a.id) - int.parse(b.id));
+  return groups;
 }
